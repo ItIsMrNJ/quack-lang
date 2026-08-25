5cfd534ef865b0121927b5405e1edd6f12674bd9
NoobJoon <its.noobjoonreal@gmail.com>
2026-08-25 11:04:46 +0430

    Quack has been fixed

--- src/quack.cpp
+++ src/quack.cpp
@@ -2,69 +2,113 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <string>
 #include "lexer.hpp"
 #include "parser.hpp"
 
 using namespace std;
 
+static void printUsage(const char *progName)
+{
+    cout << "Usage: " << progName << " <source_file> [-d | --debug]" << endl;
+}
+
+static string tokenTypeName(enum type t)
+{
+    switch (t)
+    {
+        case TOKEN_ID:              return "ID";
+        case TOKEN_INT:             return "INT";
+        case TOKEN_EQUALS:          return "EQUALS";
+        case TOKEN_SEMICOLON:       return "SEMICOLON";
+        case TOKEN_LEFT_PAREN:      return "LEFT_PAREN";
+        case TOKEN_RIGHT_PAREN:     return "RIGHT_PAREN";
+        case TOKEN_PLUS:            return "PLUS";
+        case TOKEN_MINUS:           return "MINUS";
+        case TOKEN_MULTIPLY:        return "MULTIPLY";
+        case TOKEN_DIVIDE:          return "DIVIDE";
+        case TOKEN_STRING:          return "STRING";
+        case TOKEN_COMMA:           return "COMMA";
+        case TOKEN_LEFT_BRACE:      return "LEFT_BRACE";
+        case TOKEN_RIGHT_BRACE:     return "RIGHT_BRACE";
+        case TOKEN_EQUALS_EQUALS:   return "EQUALS_EQUALS";
+        case TOKEN_LESS:            return "LESS";
+        case TOKEN_GREATER:         return "GREATER";
+        case TOKEN_LESS_EQUALS:     return "LESS_EQUALS";
+        case TOKEN_GREATER_EQUALS:  return "GREATER_EQUALS";
+        case TOKEN_PLUS_EQUALS:     return "PLUS_EQUALS";
+        case TOKEN_MINUS_EQUALS:    return "MINUS_EQUALS";
+        case TOKEN_MULTIPLY_EQUALS: return "MULTIPLY_EQUALS";
+        case TOKEN_DIVIDE_EQUALS:   return "DIVIDE_EQUALS";
+        default:                    return "UNKNOWN";
+    }
+}
+
 int main(int argc, char **argv)
 {
     if (argc < 2)
     {
-        cout << "Please supply the source file." << endl;
+        printUsage(argv[0]);
         return 1;
     }
 
-    cout << "Reading from the file name: " << argv[1] << endl;
-    ifstream sourceFileStream(argv[1]);
+    string sourcePath = argv[1];
+    bool debugMode = false;
 
-    stringstream buffer;
-    char temp;
-    while (sourceFileStream.get(temp))
+    for (int i = 2; i < argc; i++)
     {
-        buffer << temp;
+        string arg = argv[i];
+        if (arg == "-d" || arg == "--debug")
+            debugMode = true;
     }
 
+    ifstream sourceFileStream(sourcePath);
+    if (!sourceFileStream.is_open())
+    {
+        cout << "[ERROR] Could not open source file: " << sourcePath << endl;
+        return 1;
+    }
+
+    stringstream buffer;
+    buffer << sourceFileStream.rdbuf();
     string sourceCode = buffer.str();
-    cout << "-=This is a SourceCode=-" << endl << sourceCode << endl;
+    sourceFileStream.close();
+
+    if (debugMode)
+    {
+        cout << "Reading from the file name: " << sourcePath << endl;
+        cout << "-= Source Code =-" << endl << sourceCode << endl;
+    }
 
     Lexer lexer(sourceCode);
     vector<Token*> tokens = lexer.tokenize();
 
-    cout << "=== Tokens ===" << endl;
-    int counter = 0;
-    for (Token* t : tokens)
+    if (debugMode)
     {
-        counter++;
-        cout << counter << ") " << t->TYPE << " : " << t->VALUE << endl;
+        cout << "=== Tokens ===" << endl;
+        int counter = 0;
+        for (Token* t : tokens)
+        {
+            counter++;
+            cout << counter << ") " << tokenTypeName(t->TYPE) << " : " << t->VALUE << endl;
+        }
+        cout << endl;
     }
-    cout << endl;
 
     Parser parser(tokens);
     vector<Stmt*> ast = parser.parse();
 
-    ofstream outputFile("output.txt");
-    if (!outputFile.is_open()) {
-        cout << "Failed to open output file." << endl;
-        return 1;
-    }
-
+    // Program output (quack(...) calls) now prints straight to the console.
     Interpreter interpreter;
-    
-    streambuf* oldCout = cout.rdbuf();
-    cout.rdbuf(outputFile.rdbuf());
-
     interpreter.interpret(ast);
 
-    cout.rdbuf(oldCout);
-    outputFile.close();
-
     for (Stmt* stmt : ast)
-    {
         delete stmt;
-    }
+    for (Token* t : tokens)
+        delete t;
 
-    cout << "-= End of Program =-" << endl;
-    
+    if (debugMode)
+        cout << "-= End of Program =-" << endl;
+
     return 0;
 }
